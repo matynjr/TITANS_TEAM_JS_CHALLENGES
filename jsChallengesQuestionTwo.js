e8a121a6611dc2be5e993d0cdedc6b6418156227
matynjr <akua@AKUAs-MacBook-Pro.local>
2023-07-06 19:36:10 +0300

    firt 3 solutions

---
+++ jsChallengesQuestionTwo.js
@@ -0,0 +1,7 @@
+let birthYear = prompt("Enter Year of Birth");
+let currentYear = new Date().getFullYear();
+if (currentYear - birthYear <= 18) {
+  console.log("You are still a Minor");
+} else if (currentYear - birthYear <= 36) {
+  console.log("You are a Youth");
+} else console.log("You are an elder");
